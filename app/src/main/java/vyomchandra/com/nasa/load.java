065328573de41618f92dc4caf96aebb43c3cce48
vyom chandra gallani <vyomchandra@gmail.com>
2019-09-20 14:13:36 +0530

    Project for disaster todo

---
+++ app/src/main/java/vyomchandra/com/nasa/load.java
@@ -0,0 +1,14 @@
+package vyomchandra.com.nasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class load extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
